0d8cfd9435972a04860362c748f4549d738b9436
Ike Odira <odiraike8@gmail.com>
2024-08-27 14:13:54 +0100

    react and typescript

---
+++ src/test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function test() {
+    return (React.createElement("div", null, "Hello Word"));
+}
+export default test;
---
+++ src/test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function test() {
+    return (
+        <div>Hello Word</div>
+    )
+}
+
+export default test
